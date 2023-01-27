17f8badc86500b23db95e3b9f84172b7f4db9fb8
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2023-01-27 00:33:55 -0300

    feat(Token): create type token

--- src/lib/tokens.ts
+++ src/lib/tokens.ts
@@ -1,15 +1,20 @@
-export enum Token {
+export enum TokenType {
     LEFT_BRACE = 'LEFT_BRACE',
     RIGHT_BRACE = 'RIGHT_BRACE',
     LEFT_BRACKET = 'LEFT_BRACKET',
     RIGHT_BRACKET = 'RIGHT_BRACKET',
     COLON = 'COLON',
+    QUOTE = 'QUOTE',
     COMMA = 'COMMA',
     STRING = 'STRING',
     NUMBER = 'NUMBER',
     TRUE = 'TRUE',
     FALSE = 'FALSE',
     NULL = 'NULL',
+    WHITESPACE = 'WHITESPACE'
 }
 
-export const keywords = [Token.TRUE, Token.FALSE, Token.NULL] as const;
+export type Token = {
+    type: TokenType;
+    literal: boolean | number | string | null;
+}
